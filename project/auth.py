fdb6337689923140c3c375f0963434de02e913c5
WoofWoofAlpha <carl.delmonte@students.mq.edu.au>
2025-05-06 18:35:12 +1000

    finalised

--- project/auth.py
+++ project/auth.py
@@ -6,27 +6,7 @@ from . import db, app
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
-
-@auth.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(email=email).first()
-
-    # check if the user actually exists
-    # take the user-supplied password and compare it with the stored password
-    if not user or not (user.password == password):
-        flash('Please check your login details and try again.')
-        app.logger.warning("User login failed")
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
+login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
@@ -39,14 +19,16 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    user = db.session.execute(text('SELECT * FROM user WHERE email = :email'), {'email': email}).all()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
         return redirect(url_for('auth.signup'))
+   
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     # create a new user with the form data. TODO: Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=password)
+    new_user = User(email=email, name=name, password=hashed_password)
 
     # add the new user to the database
     db.session.add(new_user)
